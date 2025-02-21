a89130a6cb4556b5be8ea8ebd245fe1769f8b9e0
gregorio <gremar@kth.se>
2025-02-21 16:11:00 +0100

    docs/fix: changed nomenclature and reordering.

--- openmpc/support/constraints.py
+++ openmpc/support/constraints.py
@@ -1,10 +1,30 @@
 import numpy as np
 
 class Constraint:
-    def __init__(self, A : np.ndarray , b : np.ndarray, is_hard : bool =True, penalty_weight : float | None = None):
+    """
+    Simple class to represent polyhedral constraints of the form Hx <= b.
+    
+    """
+    def __init__(self, H : np.ndarray , b : np.ndarray, is_hard : bool =True, penalty_weight : float | None = None):
+
+        """
+        Constructor for the Constraint class.
+
+        Define constraints of the form Hx <= b. Penality is given for soft constraints in MPC optimization.
+        
+        :param H: The matrix H in the constraint Hx <= b.
+        :type H: np.ndarray
+        :param b: The vector b in the constraint Hx <= b.
+        :type b: np.ndarray
+        :param is_hard: A boolean indicating whether the constraint is hard or soft.
+        :type is_hard: bool
+        :param penalty_weight: The penalty weight for the constraint. Only used if is_hard is False.
+        :type penalty_weight: float
+
+        """
         
         
-        self.A       = A
+        self.H       = H
         self.b       = b.flatten()
         self.is_hard = is_hard
         self.penalty_weight = penalty_weight  # Only used if is_hard is False
@@ -12,9 +32,9 @@ class Constraint:
         if self.penalty_weight < 0:
             raise ValueError("penalty_weight must be non-negative.")
 
-        if len(b) != A.shape[0]:
-            raise ValueError("Number of rows in A must match the length of b. A has shape {} and b has length {}.".format(A.shape, len(b)))
+        if len(b) != H.shape[0]:
+            raise ValueError("Number of rows in A must match the length of b. A has shape {} and b has length {}.".format(H.shape, len(b)))
 
     def to_polytope(self):
         """Returns the polytope representation."""
-        return self.A, self.b
+        return self.H, self.b
