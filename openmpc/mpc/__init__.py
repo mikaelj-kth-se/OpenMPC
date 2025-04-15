45e160155c35a6c1a164939e4fc525fb5085f1f9
gregoriomarchesini <gregorio.marchesini@gmail.com>
2025-04-15 22:03:11 +0200

    fix:temp remove

--- openmpc/mpc/__init__.py
+++ openmpc/mpc/__init__.py
@@ -1,6 +1,6 @@
-from .linear_mpc    import MPC, SetPointTrackingMPC
+from .linear_mpc    import MPC, SetPointTrackingMPC, TimedMPC
 from .nonlinear_mpc import NMPC, SetPointTrackingNMPC
 from .parameters    import MPCProblem
 
-__all__ = ['MPC', 'NMPC', 'MPCProblem', 'SetPointTrackingMPC', 'SetPointTrackingNMPC']
+__all__ = ['MPC', 'NMPC', 'MPCProblem', 'SetPointTrackingMPC', 'SetPointTrackingNMPC', 'TimedMPC']
 
