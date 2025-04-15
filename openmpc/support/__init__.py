f41270b04661e20394f25b4819409fb99c6ac356
gregorio <gremar@kth.se>
2025-04-15 07:20:47 +0200

    feat:timed-mpc update

--- openmpc/support/__init__.py
+++ openmpc/support/__init__.py
@@ -1,4 +1,4 @@
 from openmpc.support.integrators import RK, forward_euler, Integrator
-from openmpc.support.constraints import Constraint
+from openmpc.support.constraints import Constraint,TimedConstraint
 
-__all__ = ['RK', 'forward_euler', 'Integrator', 'Constraint']
+__all__ = ['RK', 'forward_euler', 'Integrator', 'Constraint','TimedConstraint']
